fd6d008c55256dae645524ed31644ca772046c45
jnmii <mbuyambajoseph@gmail.com>
2023-05-27 19:30:29 -0400

    added some files to prject

---
+++ GetTarget/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GetTarget
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadIdTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.targetLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadIdTextBox
+            // 
+            this.loadIdTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loadIdTextBox.Location = new System.Drawing.Point(87, 68);
+            this.loadIdTextBox.Name = "loadIdTextBox";
+            this.loadIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loadIdTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Target";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // targetLabel
+            // 
+            this.targetLabel.AutoSize = true;
+            this.targetLabel.Location = new System.Drawing.Point(104, 164);
+            this.targetLabel.Name = "targetLabel";
+            this.targetLabel.Size = new System.Drawing.Size(38, 13);
+            this.targetLabel.TabIndex = 3;
+            this.targetLabel.Text = "Target";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 260);
+            this.Controls.Add(this.targetLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadIdTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loadIdTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label targetLabel;
+    }
+}
+
---
+++ GetTarget/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace GetTarget
+{
+    public partial class Form1 : Form
+    {
+        // Dictionary will be used as storage as long as the Id was used once and the app is still running
+        private Dictionary<string, string> getTarget;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getTarget = new Dictionary<string, string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetTargetFile("C:\\GetTarget\\TextFile1.txt");
+        }
+
+        private void GetTargetFile(string path)
+        {
+            // Confirm existence of the file first
+            if (File.Exists(path))
+            {
+                try
+                {
+                    // Get into file and read each line
+                    var lines = File.ReadAllLines(path);
+                    foreach (var line in lines)
+                    {
+                        // Split each column on the ';'
+                        var columns = line.Split(';');
+                        if (columns.Length >= 2)
+                        {
+                            var id = columns[0];
+                            var target = columns[1];
+                            // Stores the ID and target match in the dictionary so it's saved
+                            getTarget[id] = target;
+                        }
+                    }
+                }
+                // If an error occurs (no file/ bad file), show this error in a message box and inlcude an OK button
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading targets from file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //the event handler will 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //id text in the text box from user
+            string id = loadIdTextBox.Text;
+            //if theres a matching ide to target
+            if (getTarget.ContainsKey(id))
+            {
+                //display target on the target Label as text
+                string target = getTarget[id];
+                targetLabel.Text = target;
+            }
+            else
+            {
+                // If the ID isn't found, this sets the target at the clearing station
+                string clearingStationTarget = "Clearing Station";
+                targetLabel.Text = clearingStationTarget;
+            }
+
+        }
+    }
+}
